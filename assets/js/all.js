7bf2399be4863e8b5924e7e9ccaa6e2b5784109a
Pinjung Liao <pinjung.liao@gmail.com>
2021-12-03 22:50:23 +0800

    Update 2021-12-03T14:50:14.496Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,34 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$('.menu-icon').click(function (e) {
+  e.preventDefault();
+  $('.menu__list').toggleClass('active');
+});
+
+function menuScrollInit() {
+  var target = document.getElementsByClassName("nav__bar");
+
+  if (scrollY >= 600) {
+    target[0].classList.add("nav__bar--active");
+  } else {
+    target[0].classList.remove("nav__bar--active");
+  }
+} // menuScrollInit();
+
+
+window.addEventListener("scroll", menuScrollInit);
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
+}); // layout Masonry after each image loads
+
+$grid.imagesLoaded().progress(function () {
+  $grid.masonry();
+}); //Aos
+
+AOS.init({
+  once: true,
+  duration: 800
 });
 //# sourceMappingURL=all.js.map
