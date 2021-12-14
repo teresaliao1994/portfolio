689ff7550191745b3d415514bb73b08c4cff9ef8
Pinjung Liao <pinjung.liao@gmail.com>
2021-12-14 17:19:20 +0800

    “update”

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,13 +1,30 @@
 $('.menu-icon').click(function (e) {
-  e.preventDefault(); $('.menu__list').toggleClass('active')
+  e.preventDefault();
+  $('.menu__list').toggleClass('active')
 })
 
 function menuScrollInit() {
   var target = document.getElementsByClassName("nav__bar");
-  if (scrollY >= 600) {
-    target[0].classList.add("nav__bar--active");
-  } else {
-    target[0].classList.remove("nav__bar--active");
+  if (screen.width > 1400) {
+    if (scrollY >= 700) {
+      target[0].classList.add("nav__bar--active");
+    } else {
+      target[0].classList.remove("nav__bar--active");
+    }
+  }
+  else if (screen.width > 1200) {
+    if (scrollY >= 500) {
+      target[0].classList.add("nav__bar--active");
+    } else {
+      target[0].classList.remove("nav__bar--active");
+    }
+  }
+  else {
+    if (scrollY >= 360) {
+      target[0].classList.add("nav__bar--active");
+    } else {
+      target[0].classList.remove("nav__bar--active");
+    }
   }
 }
 // menuScrollInit();
@@ -27,6 +44,10 @@ $grid.imagesLoaded().progress(function () {
 });
 
 
+//Lightbox
+lightbox.option({
+  'fitImagesInViewport': true
+})
 
 
 //Aos
